f2e31f9b5bf1c3ca8fe6e00df3eed66837511151
matthewpipie <matthewpipie@gmail.com>
2016-11-08 09:25:43 -0600

    working

---
+++ js/activities.js
@@ -0,0 +1,18 @@
+// %10,2 means 8, 9, 10, 11, or 12
+var activities = [
+	"%10,2 jumping jacks.",
+	"%11,5 pushups.",
+	"%20,10 seconds of plank.",
+	"Run in place for %25,8 seconds.",
+	"Do %15,5 high knees for each leg.",
+	"Take a stretch.",
+	"%9,3 wall pushups.",
+	"Spin in a circle %3,1 times to the left, then %3,1 times to the right.",
+	"Make %10,3 small circles with your arms.",
+	"Jump over a pencil %10,3 times.",
+	"Jump as high as you can %8,3 times.",
+	"Do %10,4 situps",
+	"Do %10,4 crunches",
+	"Take a fighting stance, jab %10,5 times, and cross %10,5 times.",
+	""
+]
