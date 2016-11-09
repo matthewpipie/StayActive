f80592ed3a7160b8c588c73d4f31da1909ce7ddf
matthewpipie <matthewpipie@gmail.com>
2016-11-08 19:36:12 -0600

    Working logic for getRandomActivity()! Updated activities

--- js/activities.js
+++ js/activities.js
@@ -2,31 +2,52 @@
 var activities = [
 	"Do %15,5 jumping jacks.",
 	"Do %11,5 pushups.",
-	"Do %20,10 seconds of plank.",
+	"Do %25,15 seconds of plank.",
 	"Run in place for %25,8 seconds.",
 	"Do %15,5 high knees for each leg.",
 	"Take a stretch.",
-	"Do %9,3 wall pushups.",
+	"Do %20,5 wall pushups.",
 	"Spin in a circle %3,1 times to the left, then %3,1 times to the right.",
 	"Make %10,3 small circles with your arms.",
 	"Jump over a pencil %8,3 times.",
 	"Jump as high as you can %8,3 times.",
 	"Do %10,4 situps",
 	"Do %10,4 crunches",
-	"Take a fighting stance, jab %10,5 times, and cross %10,5 times.",
+	"Take a fighting stance, jab %13,4 times, and cross %12,5 times.",
 	"Climb %9,3 rungs of an invisible ladder.",
 	"Wall sit for %15,5 seconds.",
 	"Hop from your left foot to your right foot %15,4 times.",
 	"Pedal your legs for %14,2 seconds.",
 	"Do %13,3 squats.",
 	"Do %11,5 tricep dips.",
-	"Walk around the room %2,1 times.",
-	"Run around the room %2,1 times.",
+	"Walk around the room %3,1 times.",
+	"Run around the room %3,1 times.",
 	"Do %11,3 step-ups onto a chair.",
 	"Do %10,3 lunges.",
 	"Do %6,2 pushups with rotation.",
-	"Do %15,3 seconds of side plank",
-	"Stand up and touch your toes %8,3",
+	"Do %15,3 seconds of side plank.",
+	"Stand up and touch your toes %8,3 times.",
 	"Lay down and do %10,2 bicycle crunches.",
-	""
+	"March in place for %30,20 steps."
 ]
+
+var regex = /%\d+,\d+/g;
+
+function getRandomActivity() {
+	var activityUnformatted = activities[Math.floor(Math.random() * activities.length)];
+	console.log(activityUnformatted);
+	var matches = [];
+	while (match = regex.exec(activityUnformatted)) {
+		var str = match["0"];
+		var strNumbers = str.split("%")[1].split(",");
+		matches.push({at: match.index, median: parseInt(strNumbers[0], 10), range: parseInt(strNumbers[1], 10), length: str.length});
+	}
+	console.log(matches);
+	var activityFormatted = activityUnformatted;
+	for (var i = matches.length - 1; i >= 0; i--) {
+		console.log(i);
+		var reps = Math.floor(Math.random() * (1 + matches[i]['range'])) * 2.0 - matches[i]['range'] + matches[i]['median'];
+		activityFormatted = activityFormatted.substr(0,matches[i]['at']) + reps + activityFormatted.substr(matches[i]['at'] + matches[i]['length'], activityFormatted.length);
+	} //at the end, activityFormatted is formatted
+	console.log(activityFormatted);
+}
